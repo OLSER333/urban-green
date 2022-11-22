120a02da652911b090636fcd59112b7b41236885
olser <gritsienko.oleg@mail.ru>
2022-11-22 17:48:04 +0300

    fixed some bugs

--- src/js/components/servicesItem.js
+++ src/js/components/servicesItem.js
@@ -6,16 +6,17 @@ export function hintWork() {
 
   const items = document.querySelectorAll(".services__list-item");
   for (let item of items) {
-    // item.addEventListener("mouseenter", =  () => {
-    // hint.innerText = this.dataset.description;
-    // hint.style.display = "inline-block";
-    // });
+    item.addEventListener("mouseenter", () => {
+      // hint.innerText = this.dataset.description;
+      // hint.style.display = "inline-block";
+      hint.style.transform = `scale(1) translateY(0px) `;
+    });
     item.addEventListener("mouseleave", () => {
-      hint.style.transform = "scale(0)";
+      hint.style.transform = `scale(0) translateY(-200px) `;
     });
     item.addEventListener("mousemove", (e) => {
       console.log("e", e);
-      hint.style.transform = "scale(1)";
+
       if (e.pageX + hint.offsetWidth + hintW < document.body.offsetWidth) {
         hint.style.top = e.pageY + -hintW + "px";
         hint.style.left = e.pageX + 10 + "px";
