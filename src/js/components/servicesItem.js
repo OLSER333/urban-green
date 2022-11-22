437eefe971905de6c780ee92f1f81ff621e7c920
olser <gritsienko.oleg@mail.ru>
2022-11-22 18:33:49 +0300

    anim similar color department

--- src/js/components/servicesItem.js
+++ src/js/components/servicesItem.js
@@ -1,12 +1,13 @@
 export function hintWork() {
-  const hint = document.createElement("div");
   const hintW = 100;
-  hint.className = "hint";
-  document.querySelector(".services__list").appendChild(hint);
+  const hints = document.querySelectorAll(".services__hint");
 
   const items = document.querySelectorAll(".services__list-item");
-  for (let item of items) {
-    item.addEventListener("mouseenter", () => {
+  for (let i = 0; i < items.length; i++) {
+    const hint = hints[i];
+    const item = items[i];
+    item.addEventListener("mouseenter", (e) => {
+      console.log(e);
       // hint.innerText = this.dataset.description;
       // hint.style.display = "inline-block";
       hint.style.transform = `scale(1) translateY(0px) `;
@@ -17,10 +18,16 @@ export function hintWork() {
     item.addEventListener("mousemove", (e) => {
       console.log("e", e);
 
-      if (e.pageX + hint.offsetWidth + hintW < document.body.offsetWidth) {
+      if (e.pageX + hintW * 1.5 < document.body.offsetWidth) {
+        // hint.style.transform = `translate(${e.pageX + 10}px, ${
+        //   e.pageY + -hintW
+        // }px)`;
         hint.style.top = e.pageY + -hintW + "px";
         hint.style.left = e.pageX + 10 + "px";
       } else {
+        // hint.style.transfrom = `translate(${e.pageY - hintW}px, ${
+        //   e.pageX - hint.offsetWidth - 10
+        // }px)`;
         hint.style.top = e.pageY - hintW + "px";
         hint.style.left = e.pageX - hint.offsetWidth - 10 + "px";
       }
